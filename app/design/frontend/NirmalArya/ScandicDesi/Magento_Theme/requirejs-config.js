5450dadb72ab1a7f6cbe8e6b2fcbf4835cdc8ee2
Kanhaiya Lal <kanhaiya5590@gmail.com>
2017-09-01 20:56:31 +0530

    added missing file for cat section in home page

---
+++ app/design/frontend/NirmalArya/ScandicDesi/Magento_Theme/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    "map": {
+        "*": {
+            "OwlCarousel": "Magento_Theme/owl-carousal2/js/owl-carousel2"
+        }
+    },
+    "shim":{
+		"Magento_Theme/owl-carousal2/js/owl.carousel.min": ['jquery',"jquery/jquery-migrate"]		
+    }
+};
