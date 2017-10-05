b8e36a6adf2b6d49d46251fdb5cba9cb7c158a1a
kanhaiya lal <kanhaiya5590@gmail.com>
2017-10-05 08:50:03 +0530

    cart free shipping custom offer ...

--- app/code/ScandicDesi/Shipping/view/frontend/web/js/view/cart-offers.js
+++ app/code/ScandicDesi/Shipping/view/frontend/web/js/view/cart-offers.js
@@ -2,15 +2,20 @@
  * Copyright © 2013-2017 ScandicDesi. All rights reserved.
  */
 define([
+    'jquery',
     'uiComponent',
     'Magento_Customer/js/customer-data'
-], function (Component, customerData) {
+], function ($,Component, customerData) {
     'use strict';
 
     return Component.extend({
         initialize: function () {
             this._super();
             this.cartOffers = customerData.get('cart-offers');
+            
+            if(this.cartOffers) {
+                $('.block-cart-offer .message-area').addClass('success');
+            }
         }
     });
 });
