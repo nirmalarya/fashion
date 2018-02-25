745a5d519c78d0e256653ed89e3af72d10dc2859
Nirmal Arya <hello@nirmalarya.com>
2018-02-25 02:15:13 -0500

    theme namespace change and delete unwanted modules.

--- app/design/frontend/ScandicCloud/ScandicDesi/registration.php
+++ app/design/frontend/ScandicCloud/ScandicDesi/registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/NirmalArya/ScandicDesi',
+    'frontend/ScandicCloud/ScandicDesi',
     __DIR__
 );
